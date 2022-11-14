e5afb2da516135c8b1e22f92c3ec056fb58e4667
BrucelD14 <samuderaduta144@gmail.com>
2022-11-14 21:13:12 +0700

    add TodoModal

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Button, { SelectButton } from "./Button";
 import styles from "../styles/modules/app.module.scss";
+import TodoModal from "./TodoModal";
 
 function AppHeader() {
   return (
     <div className={styles.appHeader}>
-      <Button variant="primary">Click Me</Button>
+      <Button variant="primary">Add Task</Button>
       <SelectButton id="status">
         <option value="all">ALL</option>
         <option value="incomplete">Incomplete</option>
         <option value="complete">Complete</option>
       </SelectButton>
+      <TodoModal />
     </div>
   );
 }
---
+++ src/components/TodoModal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "../styles/modules/modal.module.scss";
+
+function TodoModal() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <h1>TODO MODAL</h1>
+      </div>
+    </div>
+  );
+}
+
+export default TodoModal;
