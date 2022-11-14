86a20679dbad91cd7ba1e7d6f8eac6f05cc9ea65
BrucelD14 <samuderaduta144@gmail.com>
2022-11-14 22:37:26 +0700

    add function handleSubmit, catch value input

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Button, { SelectButton } from "./Button";
 import styles from "../styles/modules/app.module.scss";
 import TodoModal from "./TodoModal";
 
 function AppHeader() {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <div className={styles.appHeader}>
-      <Button variant="primary">Add Task</Button>
+      <Button variant="primary" onClick={() => setModalOpen(true)}>Add Task</Button>
       <SelectButton id="status">
         <option value="all">ALL</option>
         <option value="incomplete">Incomplete</option>
         <option value="complete">Complete</option>
       </SelectButton>
-      <TodoModal />
+      <TodoModal modalOpen={modalOpen} setModalOpen={setModalOpen} />
     </div>
   );
 }
