77e8f66c617189b5cb728e464b10637e69928c16
BrucelD14 <samuderaduta144@gmail.com>
2022-11-14 21:00:55 +0700

    fixed name

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -6,7 +6,7 @@ function AppHeader() {
   return (
     <div className={styles.appHeader}>
       <Button variant="primary">Click Me</Button>
-      <SelectButton>
+      <SelectButton id="status">
         <option value="all">ALL</option>
         <option value="incomplete">Incomplete</option>
         <option value="complete">Complete</option>
