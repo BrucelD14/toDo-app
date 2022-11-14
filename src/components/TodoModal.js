b0c1a789dab336362e853ca5fc586e9fadb84664
BrucelD14 <samuderaduta144@gmail.com>
2022-11-14 21:22:28 +0700

    creating todoModal

--- src/components/TodoModal.js
+++ src/components/TodoModal.js
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "../styles/modules/modal.module.scss";
+import {MdOutlineClose} from 'react-icons/md';
 
 function TodoModal() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
+        <div className={styles.closeButton}>
+        <MdOutlineClose></MdOutlineClose>
+        </div>
         <h1>TODO MODAL</h1>
       </div>
     </div>
