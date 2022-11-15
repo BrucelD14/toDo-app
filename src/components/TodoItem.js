74bc796955bfd89f2bffdfc429b9dd27d1553320
BrucelD14 <samuderaduta144@gmail.com>
2022-11-15 15:56:05 +0700

    add icon remove and edit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../styles/modules/todoItem.module.scss";
 import { getClasses } from "../utils/getClasses";
 import { format } from "date-fns/esm";
+import { MdDelete, MdEdit } from "react-icons/md";
 
 function TodoItem({ todo }) {
   return (
@@ -23,7 +24,12 @@ function TodoItem({ todo }) {
         </div>
       </div>
       <div className={styles.todoActions}>
-      <div className={styles.icon}></div>
+        <div className={styles.icon}>
+          <MdDelete />
+        </div>
+        <div className={styles.icon}>
+          <MdEdit />
+        </div>
       </div>
     </div>
   );
