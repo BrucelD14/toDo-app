e3421959d8bacc51c39d4f6a383c34c4622a84d2
BrucelD14 <samuderaduta144@gmail.com>
2022-11-15 10:47:39 +0700

    add reducer

--- src/slices/todoSlice.js
+++ src/slices/todoSlice.js
@@ -1,18 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const getInitialTodo = () => {
-    const localTodoList = window.localStorage.getItem('todoList');
-    if(localTodoList) {
-        return JSON.parse(localTodoList)
-    }
-}
+  const localTodoList = window.localStorage.getItem("todoList");
+  if (localTodoList) {
+    return JSON.parse(localTodoList);
+  }
+};
 
 const initialValue = {
-    todoList: getInitialTodo(),
-}
+  todoList: getInitialTodo(),
+};
 
 export const todoSlice = createSlice({
-    name: 'todo',
-    initialState: initialValue,
-    
-})
+  name: "todo",
+  initialState: initialValue,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todoList.push(action.payload);
+      const todoList = window.localStorage.getItem("todoList");
+      if (todoList) {
+        const todoListArr = JSON.parse(todoList);
+        todoListArr.push({
+          ...action.payload,
+        });
+        window.localStorage.setItem("todoList", JSON.stringify(todoListArr));
+      } else {
+        window.localStorage.setItem(
+          "todoList",
+          JSON.stringify([{ ...action.payload }])
+        );
+      }
+    },
+  },
+});
