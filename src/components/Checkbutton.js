c73bd0d7f269d91000cf13c1a137b6cbe1b073dd
BrucelD14 <samuderaduta144@gmail.com>
2022-11-15 22:35:06 +0700

    add checkbox

--- src/components/Checkbutton.js
+++ src/components/Checkbutton.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import styles from "../styles/modules/todoItem.module.scss";
 
 function Checkbutton() {
   return (
-    <div>checkbox</div>
-  )
+    <div className={styles.svgBox}>
+      <svg viewBox="0 0 53 38" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.svg}>
+        <path
+          fill="none"
+          strokeMiterlimit="10"
+          strokeWidth="6"
+          d="M1.5 22L16 36.5L51.5 1"
+          strokeLinejoin="round"
+          strokeLinecap="round"
+        ></path>
+      </svg>
+    </div>
+  );
 }
 
-export default Checkbutton
+export default Checkbutton;
