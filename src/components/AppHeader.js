391592d0b4b84b250c549a83ceb33c2116de7089
BrucelD14 <samuderaduta144@gmail.com>
2022-11-16 11:25:18 +0700

    add reducer filter

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -2,15 +2,25 @@ import React, { useState } from "react";
 import Button, { SelectButton } from "./Button";
 import styles from "../styles/modules/app.module.scss";
 import TodoModal from "./TodoModal";
+import { useDispatch, useSelector } from "react-redux";
+import { updateFilterStatus } from "../slices/todoSlice";
 
 function AppHeader() {
   const [modalOpen, setModalOpen] = useState(false);
+  const initialFilterStatus = useSelector((state) => state.todo.filterStatus);
+  const [filterStatus, setFilterStatus] = useState(initialFilterStatus);
+  const dispatch = useDispatch();
+
+  const updateFilter = (e) => {
+    dispatch(updateFilterStatus(e.target.value));
+  };
+
   return (
     <div className={styles.appHeader}>
       <Button variant="primary" onClick={() => setModalOpen(true)}>
         Add Task
       </Button>
-      <SelectButton id="status">
+      <SelectButton id="status" value={filterStatus} onChange={updateFilter}>
         <option value="all">ALL</option>
         <option value="incomplete">Incomplete</option>
         <option value="complete">Complete</option>
